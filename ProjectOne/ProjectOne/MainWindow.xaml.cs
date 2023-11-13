f7787de88b17d782c94e2d0210df4e7b695bb424
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2023-11-13 12:48:05 +0300

    Add files via upload

---
+++ ProjectOne/ProjectOne/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Med
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateRiskButton_Click(object sender, RoutedEventArgs e)
+        {
+            int score = CalculateRiskScore();
+            double bmi = CalculateBMI(Convert.ToDouble(txtWeight.Text), Convert.ToDouble(txtHeight.Text);
+            string riskGroup = DetermineRiskGroup(score, bmi);
+            IblResult.Content = "Группа риска (riskGroup)";
+        }
+        private int CalculateRiskScore()
+        {
+            int score = 0;
+            if(chkPreviousVTE.IsChecked == true) { }
+        }
+    }
+}
